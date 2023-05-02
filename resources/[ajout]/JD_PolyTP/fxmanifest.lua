87ca4455db1426fccc17c3f077e2c2af2a3cc35a
CtFatalv <contact.fatalv@gmail.com>
2023-05-02 21:28:32 +0200

    Legacy City

---
+++ resources/[ajout]/JD_PolyTP/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+version '1.0.0'
+lua54 'yes'
+
+game 'gta5'
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'config.lua',
+	'client.lua',
+}
+
+server_scripts {
+	'server.lua',
+	'config.lua',
+}
+
+-- PRODUCED BY JackDUpModZ 2021/2022 
+-- V1.0
+-- https://discord.gg/qvWktZPgnQ
